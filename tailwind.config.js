42bf72b9158f999fb1c4a74c39c9175cdbed27bd
Bruno Brian Sousa <bruninho_brian@hotmail.com>
2022-10-21 19:08:43 -0300

    initialize project structure

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  // future: {
+  //   removeDeprecatedGapUtilities: true,
+  //   purgeLayersByDefault: true,
+  // },
+  // purge: ["./src/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    './src/**/*.tsx'
+  ],
+  theme: {
+    extend: {
+      colors: {},
+    },
+  },
+  variants: {},
+  plugins: [],
+};
